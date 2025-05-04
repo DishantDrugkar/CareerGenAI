0ff83c8b55332070a89a252871ce6032a388525e
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-05-04 13:49:53 +0530

    Starts project

---
+++ src/test/java/com/example/CareerGenAI/CareerGenAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CareerGenAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGenAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
